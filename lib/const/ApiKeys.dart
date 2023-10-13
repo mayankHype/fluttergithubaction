8c593c16fd707441da1e198c58b01247a55b6dc2
Mayank <mayank.pandey@hypecard.in>
2023-10-13 16:18:37 +0530

    jkl

---
+++ lib/const/ApiKeys.dart
@@ -0,0 +1,13 @@
+class ApiKeys{
+
+ApiKeys._();
+static const appIdDev="eonufuc0";
+static const iOSAppIdDev="ios_sdk-9f0cdb1ee2da51fd376ffb0391c97474bdb39b5b";
+static const androidAppIdDev="android_sdk-61319c7626326e701ebebc0fc6cbf7dcb6078aae";
+static const appId="tvuzv6hl";
+static const iOSAppId="ios_sdk-590dd07bc76d1ec0565bb1106a267706456c0048";
+static const androidAppId="android_sdk-3127e241318a1b9de41c9d5f46bc11181e70ad77";
+
+
+
+}
